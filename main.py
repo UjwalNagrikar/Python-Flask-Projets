408a3a819688ccc281be45d773aba381d01dd363
UjwalNagrikar <ujjwalnagrikar@gmail.com>
2026-04-04 22:31:18 +0530

    First flsk project

---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'Hello, World!'
+
+@app.route('/api/health')
+def health():
+    return jsonify({"status": "running"})
+
+if __name__ == '__main__':
+    app.run(debug=True)
